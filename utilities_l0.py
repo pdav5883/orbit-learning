343ca8e0176f4fbb7710867e39000609e82185b3
Peter Davison <davison.pl@gmail.com>
2022-04-03 21:59:34 -0400

    L0 ball throwing viz

--- utilities_l0.py
+++ utilities_l0.py
@@ -1,8 +1,80 @@
 import matplotlib.pyplot as plt
-import math
+import numpy as np
+from scipy.integrate import solve_ivp 
+
 
 RADIUS_EARTH = 6371008.0
 MU_EARTH = 398600441500000.0
+
+def throw_ball(altitude, speed, maxtime):
+    """
+    Throw a ball from +y axis (m) in +x direction (m/s), propagate to impact or maxtime
+    """
+    r0 = np.array([0, RADIUS_EARTH + altitude, 0])
+    v0 = np.array([speed, 0, 0])
+    
+    if altitude < 1000:
+        max_step = 0.1
+    elif altitude < 100000:
+        max_step = 0.5
+    elif altitude < 200000:
+        max_step = 1
+    else:
+        max_step = 10
+    
+    trajectory, t = propagate_twobody(r0, v0, maxtime, max_step)
+    
+    x = trajectory[:, 0]
+    y = trajectory[:, 1]
+    
+    return x, y
+    
+
+def propagate_twobody(r0, v0, tmax, max_step=np.inf, use_impact=True):
+    """
+    Propagate cartesian state by integrating eom directly.
+    
+    r0: 3, t0 position
+    v0: 3, t0 velocity
+    tmax: the max propagation time for the trajectory
+    max_tstep: max allowable time step, recommend using None to allow RK to pick time steps
+    
+    Return: N,6 np array of state; N, np array of time steps
+    """
+    y0 = np.concatenate([r0, v0])
+    
+    sol = solve_ivp(twobody_eom, (0, tmax), y0, method='RK45', events=[impact_event], max_step=max_step, rtol=1e-8)
+    
+    return sol.y.transpose(), sol.t
+
+
+def twobody_eom(t, y):
+    """
+    y[0]: x position
+    y[1]: y position
+    y[2]: z position
+    y[3]: x velocity
+    y[4]: y vecolity
+    y[5]: z vecolity
+    """
+    r = np.sqrt(y[0] ** 2 + y[1] ** 2 + y[2] ** 2)
+    g = MU_EARTH / r ** 3
+
+    return np.array([
+        y[3],
+        y[4],
+        y[5],
+        -g * y[0],
+        -g * y[1],
+        -g * y[2]
+    ])
+
+
+def impact_event(t, y):
+    return y[0]**2 + y[1]**2 + y[2]**2 - RADIUS_EARTH**2
+
+impact_event.terminal = True
+impact_event.direction = -1
 
 
 # this function creates an empty 2D plot with earth at center
@@ -35,5 +107,6 @@ def add_trajectory(x, y, ax=None):
     ax.plot(x, y)
     
     return ax
+
 
 
